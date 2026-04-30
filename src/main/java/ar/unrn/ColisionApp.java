b82e9550cb8479d7885d58c0fb9af6ac7df88c00
Martín René <martinvilu@gmail.com>
2026-04-30 13:35:15 -0300

    TP completado

--- src/main/java/ar/unrn/ColisionApp.java
+++ src/main/java/ar/unrn/ColisionApp.java
@@ -1,5 +1,15 @@
 package ar.unrn;
 
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -9,6 +19,59 @@ public class ColisionApp {
     private static final int NUM_OBJETOS_A_GENERAR = 500_000;
 
     public static void main(String[] args) {
+        try {
+            Screen screen = new DefaultTerminalFactory().createScreen();
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            screen.startScreen();
+            TextGraphics textGraphics = screen.newTextGraphics();
+            textGraphics.setForegroundColor(TextColor.ANSI.RED);
+            textGraphics.setBackgroundColor(TextColor.ANSI.GREEN);
+            textGraphics.putString(10, 5, "¡¡Hola mundo!!");
+            screen.refresh();
+            TerminalSize screenSize = terminal.getTerminalSize();
+
+            //Place the cursor in the bottom right corner
+            terminal.setCursorPosition(screenSize.getColumns() - 1, screenSize.getRows() - 1);
+            terminal.setCursorPosition(10, 5);
+            terminal.putString("Programación 2");
+            terminal.newTextGraphics().drawLine(0, 10, 3, 5, 'x');
+            terminal.putCharacter('H');
+            terminal.putCharacter('e');
+            terminal.putCharacter('l');
+            terminal.putCharacter('l');
+            terminal.putCharacter('o');
+            terminal.putCharacter('!');
+            terminal.setCursorPosition(0, 0);
+
+            terminal.flush();
+            Thread.sleep(2000);
+            terminal.enableSGR(SGR.BOLD);
+            terminal.putCharacter('Y');
+            terminal.putCharacter('e');
+            terminal.putCharacter('l');
+            terminal.putCharacter('l');
+            terminal.putCharacter('o');
+            terminal.putCharacter('w');
+            terminal.putCharacter(' ');
+            terminal.putCharacter('o');
+            terminal.putCharacter('n');
+            terminal.putCharacter(' ');
+            terminal.putCharacter('b');
+            terminal.putCharacter('l');
+            terminal.putCharacter('u');
+            terminal.putCharacter('e');
+            terminal.flush();
+            Thread.sleep(2000);
+
+
+            screen.startScreen();
+
+            terminal.flush();
+            screen.refresh();
+        } catch (IOException | InterruptedException exception) {
+            System.out.println("BOOM");
+            System.exit(1);
+        }
         System.out.println("Iniciando búsqueda de colisiones de hashCode para " + NUM_OBJETOS_A_GENERAR + " objetos...");
 
         Map<Integer, List<ObjetoSimple>> objetosPorHashCode = new HashMap<>();
@@ -50,5 +113,23 @@ public class ColisionApp {
         System.out.println("\nResumen:");
         System.out.println("Total de objetos generados: " + NUM_OBJETOS_A_GENERAR);
         System.out.println("Total de colisiones de hashCode encontradas: " + colisionesEncontradas);
+    }
+
+    public static class MyWindow extends BasicWindow {
+        public MyWindow() {
+            super("My Window!");
+            Panel horizontalPanel = new Panel();
+            horizontalPanel.setLayoutManager(new LinearLayout(Direction.HORIZONTAL));
+            Panel leftPanel = new Panel();
+            Panel middlePanel = new Panel();
+            Panel rightPanel = new Panel();
+
+            horizontalPanel.addComponent(leftPanel);
+            horizontalPanel.addComponent(middlePanel.withBorder(Borders.singleLineBevel("Panel Title")));
+            horizontalPanel.addComponent(rightPanel.withBorder(Borders.doubleLineBevel()));
+
+            // This ultimately links in the panels as the window content
+            setComponent(horizontalPanel);
+        }
     }
 }
